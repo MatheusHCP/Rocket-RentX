a336451a722b23de9d4e19168897c365e49baa70
Matheus Pereira <65426315+MatheusHCP@users.noreply.github.com>
2022-06-30 17:27:48 -0300

    add(new screen Schedulling)
    * created new screen schedulling;

--- App.tsx
+++ App.tsx
@@ -3,11 +3,12 @@ import { Home } from './src/screens/Home';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/styles/theme';
 import { CarDetails } from './src/screens/CarDetails';
+import { Scheduling } from './src/screens/Scheduling';
 
 export default function App(){
 return(
     <ThemeProvider theme={theme} >
-        <CarDetails/>
+        <Scheduling/>
     </ThemeProvider>
 )
 }
