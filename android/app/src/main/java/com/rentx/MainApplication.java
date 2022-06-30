d457b3be5d99d38e44a4be1c542e7794e1fd13c4
Matheus Pereira <65426315+MatheusHCP@users.noreply.github.com>
2022-06-29 23:52:19 -0300

    add(header)
    * created header component responsive.
    * add icons in assets folder
    * created type for .svg and .png files from react-native-svg and for png default from react
    * add new library react-native-responsive-fontsize
    * add new library react-native-svg
    * add new library react-native-svg-transformer
    * add internal configuration in android and iOS from library react-native-svg

--- android/app/src/main/java/com/rentx/MainApplication.java
+++ android/app/src/main/java/com/rentx/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.soloader.SoLoader;
 import com.rentx.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.horcrux.svg.SvgPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
+          packages.add(new SvgPackage());
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
